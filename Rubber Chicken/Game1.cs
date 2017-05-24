ecc9e1b59ef61c87fe583157b1131de416edf90f
aleksacastle <aaklochko@gmail.com>
2017-05-24 14:33:34 +0300

    Добавьте файлы проекта.

---
+++ Rubber Chicken/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Rubber_Chicken
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        RubberChicken rubberChicken;
+        const int RUBBER_CHICKEN_DAMAGE = 100;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            //create rubber chicken
+            rubberChicken = new RubberChicken(
+                Content.Load<Texture2D>("rubberchicken"),
+                new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2),
+                RUBBER_CHICKEN_DAMAGE);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            MouseState mouse = Mouse.GetState();
+            // update rubber chicken
+            rubberChicken.Update(gameTime, mouse);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // draw rubber chicken
+            spriteBatch.Begin();
+            rubberChicken.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Rubber Chicken/RubberChicken.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Rubber_Chicken
+{
+    /// <summary>
+    /// A Rubber Chicken class
+    /// </summary>
+    class RubberChicken
+    {
+        #region Fields
+
+        bool active = true;
+        int damage;
+
+        //drawing and moving  support
+        Texture2D sprite;
+        Rectangle drawRectangle;
+
+        //moving support
+        const int RUBBER_CHICKEN_SPEED = 5;
+        Vector2 velocity = Vector2.Zero;
+
+        //click processing support
+
+        bool clickStarted = false;
+        bool buttonReleased = true;
+        bool moving = false;
+
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sprite">sprite</param>
+        /// <param name="location">location of center</param>
+        /// <param name="damage">damage from rubber chicken</param>
+        public RubberChicken(Texture2D sprite, Vector2 location,
+            int damage)
+        {
+            this.sprite = sprite;
+            this.damage = damage;
+
+            //build draw rectangle
+            drawRectangle = new Rectangle(
+                (int)location.X - sprite.Width / 2,
+                (int)location.Y - sprite.Height / 2,
+                sprite.Width, sprite.Height);
+
+        }
+
+        #endregion
+        #region Properties
+
+        /// <summary>
+        /// Get and sets whether the rubber chicken is active
+        /// </summary>
+        public bool Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+
+        /// <summary>
+        /// Collision Rectangle for the rubber chicken
+        /// </summary>
+        public Rectangle CollisionRectangle
+        {
+            get { return drawRectangle; }
+        }
+        /// <summary>
+        /// Gets damage from rubber chicken
+        /// </summary>
+        public int Damage
+        {
+            get { return damage; }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// updates rubber chicken, moving and launching
+        /// when clicked
+        /// </summary>
+        /// <param name="gameTime">game Time</param>
+        /// <param name="mouse">mouse state</param>
+        public void Update(GameTime gameTime, MouseState mouse)
+        {
+            //move based on the velocity
+            drawRectangle.X += (int)velocity.X * gameTime.ElapsedGameTime.Milliseconds;
+            drawRectangle.Y += (int)velocity.Y * gameTime.ElapsedGameTime.Milliseconds;
+
+            //launch on click
+            // check for mouse over rubber chicken
+            if (drawRectangle.Contains(mouse.X, mouse.Y))
+            {
+                // check for click started on rubber chicken
+                if (mouse.LeftButton == ButtonState.Pressed &&
+                    buttonReleased)
+                {
+                    clickStarted = true;
+                    buttonReleased = false;
+                }
+                else if (mouse.LeftButton == ButtonState.Released)
+                {
+                    buttonReleased = true;
+
+                    // if click finished on rubber chicken, launch as appropriate
+                    if (clickStarted)
+                    {
+                        // launch if not already moving
+                        if (!moving)
+                        {
+                            moving = true;
+                            velocity = new Vector2(0, - RUBBER_CHICKEN_SPEED);
+                        }
+
+                        clickStarted = false;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// draw rubber chicken
+        /// </summary>
+        /// <param name="spriteBatch">sprite</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite,drawRectangle,Color.White);
+        }
+        #endregion
+    }
+}
